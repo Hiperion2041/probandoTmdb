befd057ff601ca3bff349abd0240da7d6e31fc26
Tadeo <tadeomeyer@hotmail.com>
2024-04-15 19:05:44 -0300

    g

--- src/main/java/com/dtb/intentodtb/App.java
+++ src/main/java/com/dtb/intentodtb/App.java
@@ -1,5 +1,7 @@
 package com.dtb.intentodtb;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a=(int) Math.random(1,1000);
+        System.out.println(a);
     }
 }
